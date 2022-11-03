dc8588751ca1386677736da28de7b5c1dba8de6b
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-11-03 17:47:46 -0400

    Debug pagination prompt number row

--- client/components/Tests/Pagination.js
+++ client/components/Tests/Pagination.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { NextArrowIcon, PreviousArrowIcon } from '../SVG_Icons';
 import { fetchPrompts } from '../../store/prompts';
@@ -14,79 +14,80 @@ const Overlay = () => {
   );
 };
 
-const PaginatedTests = (props) => {
-  // console.log('PAgeProp', props);
-  console.log('Props', props);
-  const prompts = props.prompts;
-  // console.log('Paginated Props', props);
-  const [currentTestIx, setCurrentTestIx] = useState(
-    window.localStorage.index * 1 || 0,
-  );
+const styleOnCurrentPrompt = (currentPromptNum, promptNum) => {
+  const isCurrentPrompt = currentPromptNum === promptNum;
+  return isCurrentPrompt && 'bg-lime-400 text-slate-900 pointer-events-none';
+};
 
-  const onNext = () => {
-    setCurrentTestIx((ix) => ix + 1);
-  };
-  const onPrevious = () => {
-    setCurrentTestIx((ix) => ix - 1);
-  };
-
-  const styleOnCurrent = (ix) =>
-    ix === currentTestIx && 'bg-lime-400 text-slate-900 pointer-events-none';
-
-  useEffect(() => {
-    props.fetchPrompts();
-    window.localStorage.setItem('index', currentTestIx);
-    // console.log(currentTestIx, window.localStorage.index * 1);
-  }, [currentTestIx]);
-
+const PaginationRow = ({ currentPromptNum, prompts }) => {
   return (
-    <div className='top-0 mt-[-74px] flex h-screen max-h-screen flex-col justify-between overflow-hidden pt-[70px]'>
-      <Overlay />
-      <TestGen currentPrompt={currentTestIx} />
-      <div
-        className='flex max-h-[7vh] items-center justify-center gap-4 p-8'
-        style={{}}>
-        <Link to={`/dynamic/${currentTestIx - 1}`}>
-          <button
-            onClick={onPrevious}
-            disabled={currentTestIx === 0}
-            className='group mr-4 flex items-center gap-3 text-lime-400 hover:text-lime-600  disabled:text-slate-700'>
-            <PreviousArrowIcon />
-            Previous
-          </button>
-        </Link>
-        {prompts?.map((_test, ix) => {
-          return (
+    <div
+      className='flex max-h-[7vh] items-center justify-center gap-4 p-8'
+      style={{}}>
+      <Link to={`/dynamic/${currentPromptNum - 1}`}>
+        <button
+          disabled={currentPromptNum <= 1}
+          className='group mr-4 flex items-center gap-3 text-lime-400 hover:text-lime-600  disabled:text-slate-700'>
+          <PreviousArrowIcon />
+          Previous
+        </button>
+      </Link>
+      {prompts.map((_prompt, index) => {
+        const promptNum = index + 1;
+        const isCurrentStyle = styleOnCurrentPrompt(
+          promptNum,
+          currentPromptNum,
+        );
+        return (
+          <Link key={promptNum} to={`/dynamic/${promptNum}`}>
             <span
-              key={ix}
-              className={`${styleOnCurrent(
-                ix,
-              )}  flex h-8 w-8 cursor-pointer items-center justify-center self-center rounded-lg transition-all hover:bg-slate-600`}>
-              <Link to={`/dynamic/${ix + 1}`}>{`  ${ix + 1}  `}</Link>
+              className={`${isCurrentStyle} flex h-8 w-8 cursor-pointer items-center justify-center self-center rounded-lg transition-all hover:bg-slate-600`}>
+              {`  ${promptNum}  `}
             </span>
-          );
-        })}
-        <Link to={`/dynamic/${currentTestIx + 1}`}>
-          <button
-            onClick={onNext}
-            disabled={currentTestIx === prompts?.length - 1}
-            className='group ml-4 flex items-center gap-3 text-lg text-lime-400 transition-all hover:text-lime-600  disabled:text-slate-700'>
-            Next
-            <NextArrowIcon />
-          </button>
-        </Link>
-      </div>
+          </Link>
+        );
+      })}
+      <Link to={`/dynamic/${currentPromptNum + 1}`}>
+        <button
+          disabled={currentPromptNum === prompts.length}
+          className='group ml-4 flex items-center gap-3 text-lg text-lime-400 transition-all hover:text-lime-600  disabled:text-slate-700'>
+          Next
+          <NextArrowIcon />
+        </button>
+      </Link>
     </div>
   );
 };
 
-// Get number of prompts from local storage
+const PaginatedPrompts = (props) => {
+  const { currentPrompt, currentPromptNum, fetchPrompts, prompts } = props;
 
-// export default PaginatedTests;
+  useEffect(() => {
+    fetchPrompts();
+  }, []);
+
+  return (
+    <div className='top-0 mt-[-74px] flex h-screen max-h-screen flex-col justify-between overflow-hidden pt-[70px]'>
+      <Overlay />
+      <TestGen
+        currentPrompt={currentPrompt}
+        currentPromptNum={currentPromptNum}
+      />
+      <PaginationRow currentPromptNum={currentPromptNum} prompts={prompts} />
+    </div>
+  );
+};
+
 const mapStateToProps = (state, props) => {
+  const { match } = props;
+  const currentPromptNum = 1 * match.params.promptNum;
+  const { prompts } = state;
+  const promptIndex = currentPromptNum - 1;
+  const currentPrompt = prompts[promptIndex] || {};
   return {
-    state,
-    props,
+    currentPrompt,
+    currentPromptNum,
+    prompts,
   };
 };
 
@@ -96,4 +97,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PaginatedTests);
+export default connect(mapStateToProps, mapDispatchToProps)(PaginatedPrompts);
