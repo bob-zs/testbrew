c1d6c5eaafbd284f6a5f1dc7545b6a1fe82d1d45
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-11-06 17:55:42 -0500

    Clean up test evailation on BE

--- server/api/submitTest.js
+++ server/api/submitTest.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const router = require('express').Router();
 const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const {
   models: { TestingPrompt },
 } = require('../db');
@@ -15,29 +16,19 @@ router.post('/', async (req, res) => {
   const jsCode = findJsCode.dataValues.jsCode;
 
   if (req.body.hasTestPassed === true) {
-    req.body.id = req.body.id + '.test.js';
-    fs.writeFile(
-      './testFiles/' + req.body.id,
-      jsCode + '\n' + '\n' + req.body.code,
-      function (err) {
-        if (err) throw err;
-      },
-    );
+    const testFileName = req.body.id + '.test.js';
+
+    const filePath = `./testFiles/${testFileName}`;
+    const jsCodeWithUserCode = `${jsCode}\n\n${req.body.code}`;
 
     try {
-      const exec = util.promisify(require('child_process').exec);
-      const { stderr } = await exec(`npm test ${req.body.id}`);
+      fs.writeFileSync(filePath, jsCodeWithUserCode);
+      const { stderr } = await exec(`npm test ${testFileName}`);
       res.json(stderr.toString());
     } catch (err) {
       res.send(err.toString());
     } finally {
-      fs.unlinkSync('./testFiles/' + req.body.id, (err) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('success!');
-        }
-      });
+      fs.unlinkSync(filePath);
     }
   }
 });
