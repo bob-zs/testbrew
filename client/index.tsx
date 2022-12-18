4568f5e39c477f80a3a558a8856ce80e99f9dce9
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-12-18 15:21:06 -0500

    Render first FE file with typescript

---
+++ client/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from './history';
+import store from './store';
+import App from './App';
+import './style.css';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+);
+
